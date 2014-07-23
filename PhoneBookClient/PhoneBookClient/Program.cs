85c4ca6f0745c94e06f52aba626632515233ce02
Василий Почкаенко <pochkaenkov@yandex.ru>
2014-07-24 02:29:54 +0800

    Создал проект клиента

---
+++ PhoneBookClient/PhoneBookClient/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneBookClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
