fa2267599c892a2b81c1d92d6d60bcf71ab895d8
Василий Почкаенко <pochkaenkov@yandex.ru>
2014-07-24 01:40:55 +0800

    Создал и реализовал заглушкой контракт службы.

--- phonebookwcftest/phonebookwcftest/Service1.cs
+++ phonebookwcftest/phonebookwcftest/Service1.cs
@@ -7,25 +7,11 @@ using System.Text;
 
 namespace phonebookwcftest
 {
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
-    public class Service1 : IService1
+    public class PhoneBook : IPhoneBook
     {
-        public string GetData(int value)
-        {
-            return string.Format("You entered: {0}", value);
-        }
-
-        public CompositeType GetDataUsingDataContract(CompositeType composite)
-        {
-            if (composite == null)
-            {
-                throw new ArgumentNullException("composite");
-            }
-            if (composite.BoolValue)
-            {
-                composite.StringValue += "Suffix";
-            }
-            return composite;
+        public string GetFullNameByPhone(string phone)
+        {           
+            return "test";
         }
     }
 }
