30920e321efa045f1bf1e41455415b0e73561391
Василий Почкаенко <pochkaenkov@yandex.ru>
2014-07-24 22:31:37 +0800

    Заготовка слоя данных.

---
+++ phonebookwcftest/phonebookwcftest/PhoneBook.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneBookWcfTest
+{
+    class PhoneBookData
+    {
+        PhoneBookItem GetPhoneBookItemByPhone(string phone)
+        {
+            /// DoIt;
+            return new PhoneBookItem { Surname = "test", Name = "test", Patronymic = "test", Phone = phone };
+        }
+    }
+}
---
+++ phonebookwcftest/phonebookwcftest/PhoneBookItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneBookWcfTest
+{
+    class PhoneBookItem
+    {
+        public string Surname { get; set; }
+        public string Name { get; set; }
+        public string Patronymic { get; set; }
+
+        public string Phone { get; set; }
+    }
+}
