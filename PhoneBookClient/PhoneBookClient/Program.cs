a7f637071b2d85e728a861169c3389ce34b18397
Василий Почкаенко <pochkaenkov@yandex.ru>
2014-07-24 21:58:22 +0800

    Ссылка на службу и простая реализация клиента.

--- PhoneBookClient/PhoneBookClient/Program.cs
+++ PhoneBookClient/PhoneBookClient/Program.cs
@@ -3,13 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PhoneBookClient.PhoneBookServiceReference;
 
 namespace PhoneBookClient
 {
     class Program
     {
+        const string exitCode = "EXIT";
         static void Main(string[] args)
         {
+            var phoneBook = new PhoneBookClient.PhoneBookServiceReference.PhoneBookClient();
+
+            Console.WriteLine(Messages.InputPhoneOrExit, exitCode);
+            var userInPut = Console.ReadLine();
+            while (userInPut != exitCode)
+            {
+                Console.WriteLine(phoneBook.GetFullNameByPhone(userInPut));
+                userInPut = Console.ReadLine();
+            }
         }
     }
 }
